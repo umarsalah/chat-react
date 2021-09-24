554d474042e34353ee3d96bb47601248cdc7ca37
umar <umars.salah@gmail.com>
2021-09-25 02:02:08 +0300

    firebase & react

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+
+import firebase from "firebase/compat/app";
+
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+import "./App.css";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBxNl9Nwn17ephn4UQknnRrfaKh1VFZcjk",
+  authDomain: "chat-8f3e4.firebaseapp.com",
+  projectId: "chat-8f3e4",
+  storageBucket: "chat-8f3e4.appspot.com",
+  messagingSenderId: "290200143234",
+  appId: "1:290200143234:web:67b0c4aea9d708fda9fb9b",
+  measurementId: "G-HDX748N9YY",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App() {
+  const [user] = useAuthState(auth);
+  return (
+    <div className="App">
+      <header>
+        <SignOut />
+      </header>
+      <section>{user ? <Chat /> : <SignIn />}</section>
+    </div>
+  );
+}
+
+const SignIn = () => {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+};
+
+const SignOut = () => {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign out</button>
+  );
+};
+
+const Chat = () => {
+  const messageRef = firestore.collection("messages");
+  const query = messageRef.orderBy("createdAt").limit(25);
+
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  const [formValue, setFormValue] = React.useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser;
+
+    await messageRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+    setFormValue("");
+  };
+
+  return (
+    <>
+      <div>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </>
+  );
+};
+
+const ChatMessage = (props) => {
+  const { text, photoURL } = props.message;
+  return (
+    <div>
+      <img src={photoURL} alt="profilePhoto" />
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default App;
